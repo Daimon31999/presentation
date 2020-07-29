0752c1fc3723d5af4b82511ed7db63a1cf918983
Daimon31999 <dimahinev31999@gmail.com>
2020-07-29 15:28:49 +0300

    description edited

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `dh.md`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `dh.md - Все о Диме`,
     author: `@gatsbyjs`,
   },
   plugins: [
