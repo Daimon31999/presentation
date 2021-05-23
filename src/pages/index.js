74ae9db9fda8371bd2a90bf605259fb9c9844e78
Daimon31999 <dimahinev31999@icloud.com>
2021-05-23 14:21:33 +0300

    markdown corrected

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,19 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Всем привет!</h1>
     <p>
-      Это мой первый блог на <b><span style={{color: '#542c85'}}>Gatsby + </span> <span style={{color: '#61dafb'}}>React + </span> <span style={{color: '#38afbb'}}>Netlify</span></b> !<br/>
-      Это <b style={{color: 'red'}}>самый быстрый сайт</b> и блог, что только может существовать! <br/>
-      Для добавление поста исспользуется <b>markdown</b> синтаксис, что очень удобно <br/>
-      Также сайт исспользует <i>CI/CD</i> при деплое в Git, Html на Netlify пересобираются
+      Это пример блога на{" "}
+      <b>
+        <span style={{ color: "#542c85" }}>Gatsby + </span>{" "}
+        <span style={{ color: "#61dafb" }}>React + </span>{" "}
+        <span style={{ color: "#38afbb" }}>Netlify</span>
+      </b>{" "}
+      !<br />
+      Это <b style={{ color: "red" }}>самый быстрый сайт</b> и блог, что только
+      может существовать! <br />
+      Для добавление поста исспользуется <b>markdown</b> синтаксис, что очень
+      удобно <br />
+      Также сайт исспользует <i>CI/CD</i> при деплое в Git, Html на Netlify
+      пересобираются
     </p>
   </Layout>
 )
--- src/pages/тест/index.md
+++
@@ -1,7 +0,0 @@
----
-title: тест
-path: test
-date: 2020-07-29T16:51:51.728Z
-author: Dima Hinev
----
-![test](20190110_124250.jpg "test")
