4a065a3e47c0b1da2eea5a3557f853c7ced236c1
Daimon31999 <dimahinev31999@icloud.com>
2021-06-01 21:52:05 +0300

    fix deploy

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,26 +6,23 @@ import Layout from "../components/layout"
 export default function Template({ data }) {
   const post = data.markdownRemark
 
-  if (frontmatter === null) {
-    return null
-  } else
-    return (
-      <Layout>
-        <Link to="/blog">Назад</Link>
-        <hr />
-        <h1>{post.frontmatter.title}</h1>
-        <h4 style={{ marginBottom: "5rem" }}>
-          Написанно {post.frontmatter.author}
-          {`, `}
-          {post.frontmatter.date}
-        </h4>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: post.html,
-          }}
-        />
-      </Layout>
-    )
+  return (
+    <Layout>
+      <Link to="/blog">Назад</Link>
+      <hr />
+      <h1>{post.frontmatter.title}</h1>
+      <h4 style={{ marginBottom: "5rem" }}>
+        Написанно {post.frontmatter.author}
+        {`, `}
+        {post.frontmatter.date}
+      </h4>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: post.html,
+        }}
+      />
+    </Layout>
+  )
 }
 
 export const postQuery = graphql`
